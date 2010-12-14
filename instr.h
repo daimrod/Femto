5b9e09f88df7a41c4ca8b2dd039556dea833798b
jgreg <gregoire.jadi@etu.univ-nantes.fr>
2010-12-14 22:21:54 +0100

    Commente le header instr.h (doxygen style)

--- instr.h
+++ instr.h
@@ -1,3 +1,6 @@
+/**
+ * \file instr.h
+ */
 #ifndef _INSTR_H_
 #define _INSTR_H_
 
@@ -6,14 +9,34 @@
 #include <stdint.h>
 #include <math.h>
 
+/**
+ * \df INC_IP(instr) mod_ip(instr, 1)
+ * \brief Incrémente le pointeur instruction pour passer à l'instruction
+ * suivante
+ *
+ * \param instr le contexte de l'émulateur
+ */
 #define INC_IP(instr) mod_ip(instr, 1)
 
+/**
+ * \struct instr_s
+ * \brief Structure correspondant au contexte de l'émulateur
+ *
+ * \param ins correspond à un tableau contenant toutes les instructions
+ * du programme en cours d'exécution
+ * \param nb correspond au nombre d'instructions
+ * \param ip correspond a l'instruction courante
+ */
 typedef struct {
   uint64_t *ins;
   uint32_t nb;
   uint32_t ip;
 } instr_s;
 
+/**
+ * \df typedef void (*fp_instr)(float*, uint8_t*, instr_s*, uint8_t*)
+ * \brief Définit un alias pour un pointeur de fonction
+ */
 typedef void (*fp_instr)(float*, uint8_t*, instr_s*, uint8_t*);
 
 fp_instr*	f_init(void);
