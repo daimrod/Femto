1543538919167709b4e35c262cf562c0cba7f01c
jgreg <gregoire.jadi@etu.univ-nantes.fr>
2010-12-18 21:08:46 +0100

    Renomme le contexte en instr_sa pour ne pas confondre avec d'autre variables

--- instr.h
+++ instr.h
@@ -4,8 +4,11 @@
 #ifndef _INSTR_H_
 #define _INSTR_H_
 
+#include "util.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <stdint.h>
 #include <math.h>
 
@@ -16,7 +19,7 @@
  *
  * \param instr le contexte de l'émulateur
  */
-#define INC_IP(instr) mod_ip(instr, 1)
+#define INC_IP(instr_sa) mod_ip(instr_sa, 1)
 
 /**
  * \struct instr_s
@@ -42,19 +45,20 @@ typedef struct {
 typedef void (*fp_instr)(float*, uint8_t*, instr_s*);
 
 fp_instr*	f_init(void);
+fp_instr*	f_init_fgui(void);
 void mod_ip(instr_s *instr_sa, int32_t offset);
 
-void	f_add	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_b	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_cmp	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_load	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_move	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_mul	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_pow	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_print	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_read	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_stop	(float *reg, uint8_t *ra, instr_s *instr);
-void	f_sub	(float *reg, uint8_t *ra, instr_s *instr);
+void	f_add	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_b	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_cmp	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_load	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_move	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_mul	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_pow	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_print	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_read	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_stop	(float *reg, uint8_t *ra, instr_s *instr_sa);
+void	f_sub	(float *reg, uint8_t *ra, instr_s *instr_sa);
 
 #define AL 0
 #define EQ 1
