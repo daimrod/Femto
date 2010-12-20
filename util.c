e500da9c4db1a1a515e5e30ddafccb1b91ccea84
jgreg <gregoire.jadi@etu.univ-nantes.fr>
2010-12-20 16:25:02 +0100

    Commente util.c (doxygen)

--- util.c
+++ util.c
@@ -1,8 +1,25 @@
+/**
+ * \file util.c
+ * \brief Ce fichier contient quelques fonctions facilitant la lecture
+ * du code
+ */
+
 #include "util.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * \fn void* xmalloc(size_t size)
+ * \brief Alloue la mémoire demandée et vérifie le code retour de la
+ * fonction malloc
+ *
+ * \param size taille de la mémoire à allouer
+ * \return un pointeur sur la zone allouée
+ *
+ * S'il y a une erreur lors de l'allocation on affiche un message d'erreur
+ * et on quitte le programme.
+ */
 void* xmalloc(size_t size) {
   void* ret = NULL;
 
