9484559271e82e934d7ca88dd36f5d0e53842134
jgreg <gregoire.jadi@etu.univ-nantes.fr>
2010-12-20 16:56:01 +0100

    Commente fgui.c (doxygen)

--- fgui.c
+++ fgui.c
@@ -1,3 +1,9 @@
+/**
+ * \file fgui.c
+ * \brief Ce fichier contient les fonctions utilisées lors de
+ * l'utilisation du mode graphique avec ncurses
+ */
+
 #include "fgui.h"
 
 #include "gui.h"
@@ -7,6 +13,16 @@
 
 #include <ncurses.h>
 
+/**
+ * \fn fp_instr*	f_init_fgui(void)
+ * \brief Initialise le tableau d'instruction utilisé dans le mode
+ * graphique
+ *
+ * \return le talbeau d'instruction initialisé
+ *
+ * Cette fonction permet de supprimer les entrées print et read du
+ * tableau habituellement utilisé car on en utilisera d'autres.
+ */
 fp_instr*	f_init_fgui(void) {
   fp_instr *fp_instr_a;
 
@@ -25,56 +41,98 @@ fp_instr*	f_init_fgui(void) {
   return fp_instr_a;
 }
 
-void	f_print_fgui(float *reg, uint8_t *ra,
-		     instr_s *instr_sa, int x, int y, window *w_out) {
-  clear_line_from_to(w_out, x, get_window_width(w_out)-2, y, 0);
-  wattron(w_out, A_BOLD);
-  printfXY(w_out, x, y, 0, "%f", reg[ra[0]]);
-  wattroff(w_out, A_BOLD);
-  redraw_window(w_out);
-  redraw_window(w_out);
+/**
+ * \fn void	f_print_fgui(float *reg, uint8_t *ra, instr_s *instr_sa, window *w)
+ * \brief Remplace la fonction f_print et affiche le registre indiqué
+ * dans la fenêtre prévue.
+ *
+ * \param reg un talbeau contenant les 16 registres du processeur Femto
+ * \param ra un talbeau indiquant les registres à utilisé pour l'opération
+ * \param instr_sa le contexte courant
+ * \param w la fenêtre où afficher
+ */
+void	f_print_fgui(float *reg, uint8_t *ra, instr_s *instr_sa, window *w) {
+  clear_line_from_to(w, 1, get_window_width(w)-2, 1, 0);
+  wattron(w, A_BOLD);
+  printfXY(w, 1, 1, 0, "%f", reg[ra[0]]);
+  wattroff(w, A_BOLD);
+  redraw_window(w);
+  redraw_window(w);
 
   INC_IP(instr_sa);
 }
 
-void	f_read_fgui(float *reg, uint8_t *ra,
-		    instr_s *instr_sa, int x, int y,
-		    window *w_in) {
-  clear_line_from_to(w_in, x, get_window_width(w_in)-2, y, 0);
-  wattron(w_in, A_BOLD);
-  printfXY(w_in, x, y, 0, "Entrez un nombre: ");
-  wattroff(w_in, A_BOLD);
-  redraw_window(w_in);
+/**
+ * \fn void	f_print_fgui(float *reg, uint8_t *ra, instr_s *instr_sa, window *w)
+ * \brief Remplace la fonction f_read gèrant la saisie utilisateur
+ *
+ * \param reg un talbeau contenant les 16 registres du processeur Femto
+ * \param ra un talbeau indiquant les registres à utilisé pour l'opération
+ * \param instr_sa le contexte courant
+ * \param w la fenêtre où se passe la saisie
+ *
+ * On affiche un message dans une fenêtre demandant à l'utilisateur
+ * de rentrer un nombre.
+ */
+void	f_read_fgui(float *reg, uint8_t *ra, instr_s *instr_sa, window *w) {
+  clear_line_from_to(w, 1, get_window_width(w)-2, 1, 0);
+  wattron(w, A_BOLD);
+  printfXY(w, 1, 1, 0, "Entrez un nombre: ");
+  wattroff(w, A_BOLD);
+  redraw_window(w);
   echo();
-  wscanw(w_in, "%f", &(reg[ra[0]]));
-  printfXY(w_in, x, y, 0, "Entrez un nombre: ");
+  wscanw(w, "%f", &(reg[ra[0]]));
+  printfXY(w, 1, 1, 0, "Entrez un nombre: ");
   noecho();
-  redraw_window(w_in);
+  redraw_window(w);
 
   INC_IP(instr_sa);
 }
 
-void fgui_print_reg(float *reg, window *w_reg) {
+/**
+ * \fn void fgui_print_reg(float *reg, window *w)
+ * \brief Affiche le contenu des 16 registres du processeur Femto dans
+ * la fenêtre indiquée
+ *
+ * \param reg le tableau correspondant aux 16 registres du processeur
+ * Femto
+ * \param w la fenêtre où afficher
+ */
+void fgui_print_reg(float *reg, window *w) {
   size_t i;
 
   for (i = 0; i < 16; ++i)
-    clear_line_from_to(w_reg, 2, get_window_width(w_reg)-2, 2+i, 0);
+    clear_line_from_to(w, 2, get_window_width(w)-2, 2+i, 0);
   for (i = 0; i < 16; ++i) {
-    printfXY(w_reg, 2, 2+i, 0, "r%d: %f", i, reg[i]);
+    printfXY(w, 2, 2+i, 0, "r%d: %f", i, reg[i]);
   }
 
 }
 
-void fgui_print_prog(instr_s *instr_sa, window *w_prog) {
+/**
+ * \fn void fgui_print_prog(instr_s *instr_sa, window *w)
+ * \brief Affiche le programme courant désassemblé dans la fenêtre
+ * indiquée
+ *
+ * \param instr_sa le contexte courant
+ * \param w la fenêtre où afficher
+ *
+ * Cette fonction affiche le programme courant désassemblé en gérant
+ * le cas où la fenêtre ne serait pas assez grande pour afficher le code
+ * en entier.
+ * De plus, une flèche indique la ligne allant être exécutée. La ligne
+ * est également affichée en gras.
+ */
+void fgui_print_prog(instr_s *instr_sa, window *w) {
   char *line;
   size_t size, i, start, end;
 
-  if (instr_sa->nb <= get_window_height(w_prog) - 2) {
+  if (instr_sa->nb <= get_window_height(w) - 2) {
     size  = instr_sa->nb;
     start = 0;
     end	  = size;
   } else {
-    size  = get_window_height(w_prog) - 4;
+    size  = get_window_height(w) - 4;
     end = MIN(instr_sa->ip + size, instr_sa->nb);
     start = end - size;
   }
@@ -82,24 +140,37 @@ void fgui_print_prog(instr_s *instr_sa, window *w_prog) {
   line = (char*) xmalloc(sizeof(char) * 255);
 
   for (i = 0; i < (end-start); ++i)
-    clear_line_from_to(w_prog, 2, get_window_width(w_prog)-2, 2+i, 0);
+    clear_line_from_to(w, 2, get_window_width(w)-2, 2+i, 0);
 
   for (i = 0; i < (end-start); ++i) {
     line = desa_line(line, instr_sa->ins[i+start]);
     if (i+start == instr_sa->ip) {
-      wattron(w_prog, A_BOLD);
-      printfXY(w_prog, 2, i+2, 0, "-> ");
+      wattron(w, A_BOLD);
+      printfXY(w, 2, i+2, 0, "-> ");
     } else {
-      wattroff(w_prog, A_BOLD);
-      printfXY(w_prog, 2, i+2, 0, "++ ", i);
+      wattroff(w, A_BOLD);
+      printfXY(w, 2, i+2, 0, "++ ", i);
     }
-    printfXY(w_prog, 5, i+2, 0, line);
+    printfXY(w, 5, i+2, 0, line);
   }
 
   free(line);
   line = NULL;
 }
 
+/**
+ * \fn
+ * void fgui_init(window **w_prog, window **w_reg, window **w_out, window **w_in)
+ * \brief Initialise les différentes fenêtres gérées durant l'exécution
+ * du programme ainsi que d'autres paramètres de l'interface ncurses.
+ *
+ * \param w_prog fenêtre dans laquelle le code désassemblé sera affiché
+ * \param w_reg fenêtre dans laquelle les registres seront affichés
+ * \param w_out fenêtre servant à l'affichage des opérations de sorties
+ * du programme
+ * \param w_in fenêtre servant à l'affichage des opérations d'entrées
+ * du programme (les saisies utilisateurs)
+ */
 void fgui_init(window **w_prog,
 	       window **w_reg,
 	       window **w_out,
@@ -121,6 +192,18 @@ void fgui_init(window **w_prog,
 			get_term_height() / 4 * 3, 1);
 }
 
+/**
+ * \fn void fgui_end(window **w_prog, window **w_reg, window **w_out, window **w_in)
+ * \brief Détruit les différentes fenêtres gérées durant l'exécution
+ * du programme et restaure le terminal dans son état initial.
+ *
+ * \param w_prog fenêtre dans laquelle le code désassemblé sera affiché
+ * \param w_reg fenêtre dans laquelle les registres seront affichés
+ * \param w_out fenêtre servant à l'affichage des opérations de sorties
+ * du programme
+ * \param w_in fenêtre servant à l'affichage des opérations d'entrées
+ * du programme (les saisies utilisateurs)
+ */
 void fgui_end(window **w_prog,
 	      window **w_reg,
 	      window **w_out,
@@ -132,6 +215,15 @@ void fgui_end(window **w_prog,
   cleanup_gui();
 }
 
+/**
+ * \fn void fgui_emula(instr_s *instr_sa)
+ * \brief Fonction principale réalisant l'exécution du programme indiqué
+ *
+ * \param instr_sa le contexte courant
+ *
+ * Cette fonction exécute le exécute le code en mode pas-à-pas et
+ * affiche le contenu des registres après chaque instruction
+ */
 void fgui_emula(instr_s *instr_sa) {
   window *w_prog, *w_reg, *w_out, *w_in;
   fp_instr *fp_instr_a;
@@ -143,6 +235,7 @@ void fgui_emula(instr_s *instr_sa) {
   fgui_init(&w_prog, &w_reg, &w_out, &w_in);
 
   fp_instr_a = f_init_fgui();
+
   for (i = 0; i < 16; ++i)
     reg[i] = 0;
 
@@ -154,6 +247,7 @@ void fgui_emula(instr_s *instr_sa) {
     fgui_print_prog(instr_sa, w_prog);
     redraw_window(w_prog);
 
+    /* Affichage des registres */
     fgui_print_reg(reg, w_reg);
     redraw_window(w_reg);
 
@@ -166,10 +260,10 @@ void fgui_emula(instr_s *instr_sa) {
     if ((suf & instr_sa->flags) | !suf) {
       switch (op) {
       case 7:
-	f_print_fgui(reg, ra, instr_sa, 1, 1, w_out);
+	f_print_fgui(reg, ra, instr_sa, w_out);
 	break;
       case 8:
-	f_read_fgui(reg, ra, instr_sa, 1, 1, w_in);
+	f_read_fgui(reg, ra, instr_sa, w_in);
 	break;
       default:
 	fp_instr_a[op](reg, ra, instr_sa);
@@ -180,6 +274,7 @@ void fgui_emula(instr_s *instr_sa) {
       INC_IP(instr_sa);
   }
 
+  /* Attente de l'appuie d'une touche avant de terminer le programme */
   getchar();
 
   free(fp_instr_a);
